ec3cb5728547c0d66c764a79322560b72d7b257a
mec <injurytime.mec@gmail.com>
2015-01-12 02:51:57 +0900

    Don't aggregate bodies for manual CM detection part 3.

--- scripts/make_index.pl
+++ scripts/make_index.pl
@@ -5,26 +5,18 @@ use warnings;
 use utf8;
 use POSIX qw/:math_h/;
 
-die "raw_scene.txt doesn't exist." unless -f 'raw_scene.txt';
 die "scene.txt doesn't exist." unless -f 'scene.txt';
 die "dump/ directory doesn't exist." unless -d 'dump';
 die "index.html already exists." if -e 'index.html';
 
-open my $raw_scene_fh, '<', 'raw_scene.txt' or die;
-my @all_sccenes = <$raw_scene_fh>;
-close $raw_scene_fh;
-
 open my $scene_fh, '<', 'scene.txt' or die;
+my @all_scenes = <$scene_fh>;
+close $scene_fh;
 my %scene_type_hash;
-for my $line (<$scene_fh>) {
+for my $line (@all_scenes) {
     my ($index, $type) = (split '\s+', $line)[0, 5];
     $scene_type_hash{$index} = lc $type;
 }
-close $scene_fh;
-
-opendir my $dh, 'dump' or die;
-my @dirnames = sort grep(!/^\./, readdir($dh));
-closedir $dh;
 
 my @output = (
     '<html>',
@@ -38,14 +30,13 @@ my @output = (
     '<body>');
 my $previous_type = 'cm';
 my $current_type = 'cm';
-for my $i (0..$#dirnames) {
-    my $dirname = $dirnames[$i];
+for my $i (0..$#all_scenes) {
     if (defined $scene_type_hash{$i}) {
         $current_type = $scene_type_hash{$i};
     }
 
     my ($scene_index, $range_type, $start, $end, $changed) =
-        split '\s+', $all_sccenes[$i];
+        split '\s+', $all_scenes[$i];
     push @output, sprintf(
         '<h3>%02d: (%s) %s - %s - %s (%s)</h3>',
         $scene_index, $range_type,
@@ -54,12 +45,12 @@ for my $i (0..$#dirnames) {
 
     push @output, '<div>';
     my $template =
-        '<img width="224px" height="126px" class="%s" src="dump/%s/%d.jpg">';
+        '<img width="224px" height="126px" class="%s" src="dump/%03d/%d.jpg">';
     push @output, 
-        sprintf($template, $previous_type, $dirname, 1) .
-        sprintf($template, $previous_type, $dirname, 2) .
-        sprintf($template, $current_type, $dirname, 3) .
-        sprintf($template, $current_type, $dirname, 4);
+        sprintf($template, $previous_type, $i, 1) .
+        sprintf($template, $previous_type, $i, 2) .
+        sprintf($template, $current_type, $i, 3) .
+        sprintf($template, $current_type, $i, 4);
     push @output, '</div>';
     $previous_type = $current_type;
 }
