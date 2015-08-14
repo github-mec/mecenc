d71bf6d2f88d135ed94356e86c62aa525d2f537e
mec <injurytime.mec@gmail.com>
2015-08-14 22:37:53 +0900

    Add kana into tesseract whitelist to reduce false-positive.

--- scripts/sponsor_detector/sponsor_detector_driver.py
+++ scripts/sponsor_detector/sponsor_detector_driver.py
@@ -7,6 +7,10 @@ import subprocess
 
 _RESULT_FILENAME = 'sponsor.txt'
 _CONVERTED_FILENAME_PREFIX = 'converted_'
+_TESSERACT_WHITELIST = u''.join(
+    [unichr(c) for c in range(ord(u'あ'), ord(u'ん') + 1)] +
+    [unichr(c) for c in range(ord(u'ァ'), ord(u'ン') + 1)]
+    ) + u'提供'
 
 
 def GetSponsorMarkDumpDirname():
@@ -48,7 +52,7 @@ def IsSponsorImage(filename):
         'stdout',
         '-l', 'jpn',
         '-psm', '6',
-        '-c', u'tessedit_char_whitelist=提供'],
+        '-c', u'tessedit_char_whitelist=%s' % _TESSERACT_WHITELIST],
         env = utf8_env)
     output = output.decode('utf-8')
     return output.find(u'提') >= 0 and output.find(u'供') >= 0
